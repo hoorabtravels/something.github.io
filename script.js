ebb7f3bca7a1f33f8dce742a8f59fcceec1ed88f
CodeyoungCoder <109608095+CodeyoungCoder@users.noreply.github.com>
2022-07-19 18:56:55 +0400

    My first Website
    A reservation form

---
+++ script.js
@@ -0,0 +1,24 @@
+function Reserved(){
+    var Name = document.getElementById("Name").value;
+    alert(Name);
+    var lastname = document.getElementById("lastname").value;
+    alert(lastname);
+    var adress = document.getElementById("address").value;
+    alert(adress);
+    var city = document.getElementById("city").value;
+    alert(city);
+    var state = document.getElementById("state").value;
+    alert(state);
+    var postalcode = document.getElementById("postalcode").value;
+    alert(postalcode);
+    var country = document.getElementById("country").value;
+    alert(country);
+    var email = document.getElementById("email").value;
+    alert(email);
+    var checkin = document.getElementById("checkin").value;
+    alert(checkin);
+    var checkout = document.getElementById("checkout").value;
+    alert(checkout);
+    var phone = document.getElementById("phone").value;
+    alert(phone);
+}
